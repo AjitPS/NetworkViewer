661561386a58e862623651d5f0f49c6c1f6c6ef4
singha <singha@PC09060-ROTH.rothamsted.ac.uk>
2015-06-26 11:39:28 +0100

    css_renderer node_labels_bug not_fixed_yet

--- NetworkCanvasCyJS/build/web/javascript/networkView_cyjs.js
+++ NetworkCanvasCyJS/build/web/javascript/networkView_cyjs.js
@@ -218,9 +218,6 @@ $('#cy').cytoscape({
   
   // Layout of the Network.
 //  layout: defaultNetworkLayout,
-/*  layout: { name: 'circle', animate: false, padding: 30, avoidOverlap: true, 
-      boundingBox: undefined, handleDisconnected: true, fit: true, counterclockwise: false,
-      radius: 3, rStepSize: 2 }, */
 
   // these options hide parts of the graph during interaction.
 //  hideEdgesOnViewport: true,
