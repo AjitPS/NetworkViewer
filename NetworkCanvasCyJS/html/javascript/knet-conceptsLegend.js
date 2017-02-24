5bedd8452bf5e73795ecefecdf6997076f142fcf
singha <singha@PC09060-ROTH.rothamsted.ac.uk>
2017-02-24 17:46:04 +0000

    counts legend

--- NetworkCanvasCyJS/html/javascript/knet-conceptsLegend.js
+++ NetworkCanvasCyJS/html/javascript/knet-conceptsLegend.js
@@ -8,7 +8,7 @@
              conceptTypes.push(ele.data('conceptType'));
             }
       });
-      console.log("\t conceptTypes in this network: "+ conceptTypes +"\n");
+//      console.log("\t conceptTypes in this network: "+ conceptTypes +"\n");
 
       var conceptsHashmap= {};
       conceptTypes.forEach(function(conType,index){
@@ -18,15 +18,29 @@
           // Push count of concepts of this Type to concepts hashmap
           conceptsHashmap[conType]= conCount;
       });
-      console.log("conceptsHashmap: ");
+/*      console.log("conceptsHashmap: ");
       for(var con in conceptsHashmap) {
           console.log(con +": "+ conceptsHashmap[con]);
-         }
+         }*/
 
-        // update networkLegend_container as well
+      // update networkLegend_container.
+      var conStats= "";
+      var knetLegend= "<div id='knetLegend'>";
+      // Show concept Type icons (with total count displayed within).
+      var cnt= 0;
+      for(var con in conceptsHashmap) {
+          knetLegend= knetLegend +'<div class="knetLegend_'+ con +'" title="'+ con +'">'+ conceptsHashmap[con] +"</div>";
+          conStats= conStats + con +": "+ conceptsHashmap[con] +", \t\t";
+          cnt= cnt+1;
+          if(cnt%8===0) { knetLegend= knetLegend+ '<br/>'; }
+         }
+      knetLegend= knetLegend +"</div>";
+//      $('#networkLegend_container').empty();
+//      $('#networkLegend_container').replace(knetLegend);
+	$('#countsLegend span').text(conStats.substring(0,conStats.length-1)); // update
+
 //        var evidence = '<td>';
-        // loop
-        // get total visible and total concept count per conceptType and show concept Type icon (with total visible count displayed within) and total count next to it.
+        // Show concept Type icons (with total count displayed within).
         // Make the icon a clickable image that, on click, shows a dropdown with a button to show all by type (for this concept type).
 //        evidence = evidence+'<div class="evidence_item evidence_item_'+evidence_elements[0]+'" title="'+evidence_elements[0]+'" ><span class="dropdown_box_open" id="evidence_box_open_'+values[1].replace(".","_")+evidence_elements[0]+'">'+((evidence_elements.length)-1)+'</span>';
 //	evidence = evidence+'</td>';
